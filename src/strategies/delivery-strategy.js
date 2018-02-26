a376246ea357ea141d325fb5f81aaf20644073e0
Davide Melfi <davide.melfi.external@ynap.com>
2018-02-26 16:02:11 +0100

    added totalDistance

--- src/strategies/delivery-strategy.js
+++ src/strategies/delivery-strategy.js
@@ -8,14 +8,20 @@ const execute = (argv, data) => {
 };
 //#region Private Members
 
-const ordering = (argv, ordersObj) => ordersObj
-  .sortByTime(o => o.deliveryTime)
-  .filter(o => o.type.toLowerCase() === 'takeaway')
-  .map(o => ({ deliveryTime: o.deliveryTime, customer: o.customer, address: o.address, totalQuantity: getTotalQuantity(o.items), totalPrice: getTotalPrice(o.items) }))
-  .filterByTime(o => o.deliveryTime, argv.from, argv.to).toArray();
+const ordering = (argv, ordersObj) => {
+  let orders = ordersObj
+    .sortByTime(o => o.deliveryTime)
+    .filter(o => o.type.toLowerCase() === 'takeaway')
+    .map(o => ({ deliveryTime: o.deliveryTime, distance: o.distance, customer: o.customer, address: o.address, totalQuantity: getTotalQuantity(o.items), totalPrice: getTotalPrice(o.items) }))
+    .filterByTime(o => o.deliveryTime, argv.from, argv.to);
+  
+  return  { orders: orders.toArray(), totalDistance: getTotalDistance(orders)};
+}
 
 const getTotalQuantity = (items) => items.map(i => i.quantity).reduce((a, b) => a + b); 
 const getTotalPrice = (items) => items.map(i => i.unitPrice * i.quantity).reduce((a, b) => a + b);
+const getDistanceInKms = distance => (distance.includes('km') ? 1 : 0.001) * distance.match(/\d+/)[0];
+const getTotalDistance = (orders) => orders.map(o => getDistanceInKms(o.distance)).reduce((a, b) => a + b);
 
 //#endregion
 
