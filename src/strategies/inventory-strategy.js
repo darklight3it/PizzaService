b111a96c4be93d01a6558c48e08961a0aaa4981e
Darklight3it <darklight3it@gmail.com>
2018-03-04 19:45:39 +0100

    Added new Strategy

---
+++ src/strategies/inventory-strategy.js
@@ -0,0 +1,29 @@
+'use strict';
+import { enhanceOrders } from '../utils/orders-utils';
+
+const name = 'Inventory';
+
+const execute = (argv, data) => {
+  return ordering(argv, enhanceOrders(data.orders));
+};
+//#region Private Members
+
+const ordering = (argv, ordersObj) =>
+  ordersObj
+    .filterByTime(o => o.deliveryTime, argv.from, argv.to)
+    .flatMap(o => o.items)
+    .sortBy(o => o.name)
+    .reduce((accum, i) => {
+      if (accum.some(x => stringEquals(x.name, i.name))) {
+        accum.find(x => stringEquals(x.name, i.name)).quantity += i.quantity;
+        return accum;
+      } 
+      
+      return accum.concat(i);
+    }, []);
+
+const stringEquals = (string, anotherString) =>
+  string.toLowerCase() === anotherString.toLowerCase();
+//#endregion
+
+export default { name, execute };
---
+++ test/inventory-strategy-test.js
@@ -0,0 +1,71 @@
+'use strict';
+import sinon from 'sinon';
+import chai from 'chai';
+import strategy from '../src/strategies/inventory-strategy';
+
+describe('Inventory Strategy', () => {
+  describe('execute', () => {
+    let sandBox = sinon.sandbox.create();
+    let fakeOrder = {
+      orders: [
+        {
+          orderId: 1,
+          deliveryTime: '2017-11-26 11:00',
+          address: 'via lunga 1',
+          type: 'takeaway',
+          distance: '800 m',
+          customer: 'Marco',
+          items: [
+            { name: 'pizza', type: 'dish', quantity: 2, unitPrice: 8 },
+            { name: 'Coca Cola', type: 'beverage', quantity: 1, unitPrice: 2.5 }
+          ]
+        },
+        {
+          orderId: 2,
+          deliveryTime: '2017-11-24 11:00',
+          address: 'via lunga 2',
+          type: 'takeaway',
+          distance: '1.5 km',
+          customer: 'Davide',
+          items: [
+            { name: 'pizza', type: 'dish', quantity: 2, unitPrice: 8 },
+            { name: 'Coca Cola', type: 'beverage', quantity: 1, unitPrice: 2.5 }
+          ]
+        },
+        {
+          orderId: 3,
+          deliveryTime: '2017-11-24 11:00',
+          address: 'via lunga 3',
+          distance: '800 m',
+          type: 'pickup_in_store',
+          customer: 'Giulio',
+          items: [{ name: 'Coca Cola', type: 'beverage', quantity: 1 }]
+        }
+      ]
+    };
+
+    beforeEach(() => {
+      // stub out the `hello` method
+    });
+
+    afterEach(() => {
+      sandBox.restore();
+    });
+
+    it('should get all the items ordered if date are not provided', () => {
+      let argv = { path: 'path' };
+      let result = strategy.execute(argv, fakeOrder);
+
+      console.log(result);
+
+      chai.assert.equal(result.length, 2);
+    });
+
+    it('should get all items ordered and order them by delivery date', () => {
+      let argv = { path: 'path', from: '2017-11-24', to: '2017-11-25' };
+      let result = strategy.execute(argv, fakeOrder);
+
+      chai.assert.equal(result.length, 2);
+    });
+  });
+});
